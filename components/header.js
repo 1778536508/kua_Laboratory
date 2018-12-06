e747a40d77d9deb8d6f0b8fe72fdca43818a99d5
niedongye <niedongye@efeiyi.com>
2018-12-06 17:46:32 +0800

    已完成五个模块

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
                         <li><a href="/view/home/home.html">首页</a></li>
                         <li><a href="/view/example/example.html">案例</a></li>
                         <li><a href="/view/Incubator/Incubator.html">K + 孵化器</a></li>
-                        <li><a href="javascript:;">合作方式</a></li>
+                        <li><a href="/view/formmode/formmode.html">合作方式</a></li>
                         <li><a href="javascript:;">我们</a></li>
                     </ul>
                 </div>
