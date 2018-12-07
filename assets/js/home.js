76460fc15a4c550caac5243274eb5082c9e17694
niedongye <niedongye@efeiyi.com>
2018-12-07 17:41:52 +0800

    Completed, to be optimized

--- assets/js/home.js
+++ assets/js/home.js
@@ -12,46 +12,46 @@ $(document).ready(function(){
         if ($(window).scrollTop() > 100) {
             $("#home-list").animate({opacity: '1'}, 1000);
         }
-        if ($(window).scrollTop() > 900) {
+        if ($(window).scrollTop() > 500) {
             $("#home-antion-left1").animate({left: '200%'},800,function () {
-                $("#home-type-txt1").animate({left: '132px'},"slow",function () {
+                $("#home-type-txt1").animate({left: '132px'},200,function () {
                     $("#home-type-txt1 > p").addClass('active');
                 });
             })
         }
-        if ($(window).scrollTop() > 1600) {
+        if ($(window).scrollTop() > 1300) {
             $("#home-antion-left2").animate({left: '200%'},800,function () {
-                $("#home-type-txt2").animate({left: '132px'},"slow",function () {
+                $("#home-type-txt2").animate({left: '132px'},200,function () {
                     $("#home-type-txt2 > p").addClass('active');
                 });
             })
 
         }
-        if ($(window).scrollTop() > 2300) {
+        if ($(window).scrollTop() > 2100) {
             $("#home-antion-left3").animate({left: '200%'},800,function () {
-                $("#home-type-txt3").animate({left: '132px'},"slow",function () {
+                $("#home-type-txt3").animate({left: '132px'},200,function () {
                     $("#home-type-txt3 > p").addClass('active');
                 });
             })
 
         }
-        if ($(window).scrollTop() > 3000) {
+        if ($(window).scrollTop() > 2900) {
             $("#home-antion-left4").animate({left: '200%'},800,function () {
-                $("#home-type-txt4").animate({left: '132px'},"slow",function () {
+                $("#home-type-txt4").animate({left: '132px'},200,function () {
                     $("#home-type-txt4 > p").addClass('active');
                 });
             })
 
         }
-        if ($(window).scrollTop() > 3500) {
+        if ($(window).scrollTop() > 3700) {
             $("#home-antion-left5").animate({left: '200%'},800,function () {
-                $("#home-type-txt5").animate({left: '132px'},"slow",function () {
+                $("#home-type-txt5").animate({left: '132px'},200,function () {
                     $("#home-type-txt5 > p").addClass('active');
                 });
             })
 
         }
-        if ($(window).scrollTop() > 4000) {
+        if ($(window).scrollTop() > 4500) {
             $("#home-cooperation-name").animate({left: '120px'},"slow");
             $("#home-cooperation-btn").animate({right: '120px'}, "slow");
         }
